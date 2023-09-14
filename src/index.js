8b53688e4a0270b590514f597dc4d097feb0915a
Jeffrey Ilgen <jilgen@Jeffreys-MacBook-Air.local>
2023-09-14 19:44:59 -0400

    switched app

--- src/index.js
+++ src/index.js
@@ -3,11 +3,39 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ErrorPage from "./ErrorPage";
+import Prompts from "./Prompts";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/prompts",
+    element: <Prompts />,
+    errorElemeny: <ErrorPage />
+  }
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
+    />
+
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
